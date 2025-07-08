925a3b5f0a75d3da5ab40af54a36e05f2a233698
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-08 12:30:22 +0200

    Solution

--- src/Acronym.hs
+++ src/Acronym.hs
@@ -1,9 +1,22 @@
 module Acronym (abbreviate) where
 
+import Data.Char (isAsciiUpper)
 import Data.Text (Text)
 import qualified Data.Text as T
 
 abbreviate :: Text -> Text
 abbreviate xs =
-  let ws = T.words (T.map (\c -> if c == '-' || c == ',' then ' ' else c) xs)
-   in T.concat $ map (T.toUpper . T.take 1) ws
+  let ws = T.words (T.map (\c -> if c == '-' || c == '_' then ' ' else c) xs)
+      is = concatMap firstAndUpper ws
+   in T.toUpper $ T.concat is
+
+firstAndUpper :: Text -> [Text]
+firstAndUpper txt
+  | T.all isAsciiUpper txt && not (T.null txt) = [T.take 1 txt]
+  | otherwise =
+      case T.uncons txt of
+        Nothing -> []
+        Just (h, t) ->
+          T.singleton h : map T.singleton (T.unpack $ T.filter isUpper t)
+  where
+    isUpper = isAsciiUpper
