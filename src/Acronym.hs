9c607a23031a18f2f2c81ef6c0a65faa9e6e2dd0
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-08 10:08:14 +0200

    in progress

--- src/Acronym.hs
+++ src/Acronym.hs
@@ -1,4 +1,9 @@
 module Acronym (abbreviate) where
 
-abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+import Data.Text (Text)
+import qualified Data.Text as T
+
+abbreviate :: Text -> Text
+abbreviate xs =
+  let ws = T.words (T.map (\c -> if c == '-' || c == ',' then ' ' else c) xs)
+   in T.concat $ map (T.toUpper . T.take 1) ws
