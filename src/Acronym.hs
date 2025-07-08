4ec30d6da5f9d90769625bd5afe94881355c0e49
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-08 12:52:54 +0200

    refactor

--- src/Acronym.hs
+++ src/Acronym.hs
@@ -5,18 +5,12 @@ import Data.Text (Text)
 import qualified Data.Text as T
 
 abbreviate :: Text -> Text
-abbreviate xs =
-  let ws = T.words (T.map (\c -> if c == '-' || c == '_' then ' ' else c) xs)
-      is = concatMap firstAndUpper ws
-   in T.toUpper $ T.concat is
-
-firstAndUpper :: Text -> [Text]
-firstAndUpper txt
-  | T.all isAsciiUpper txt && not (T.null txt) = [T.take 1 txt]
-  | otherwise =
-      case T.uncons txt of
-        Nothing -> []
-        Just (h, t) ->
-          T.singleton h : map T.singleton (T.unpack $ T.filter isUpper t)
+abbreviate = T.toUpper . T.concat . map extractInitials . T.words . normalizeText
   where
-    isUpper = isAsciiUpper
+    normalizeText :: Text -> Text
+    normalizeText = T.map (\c -> if c == '-' || c == '_' then ' ' else c)
+
+    extractInitials :: Text -> Text
+    extractInitials w
+      | T.all isAsciiUpper w = T.take 1 w
+      | otherwise = T.cons (T.head w) (T.filter isAsciiUpper (T.tail w))
